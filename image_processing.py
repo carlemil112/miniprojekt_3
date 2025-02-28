6cb7b5b49ee286322ddedcbb45d36c65488389a2
anneinger <anne@ingerslev.info>
2025-02-28 14:43:05 +0100

    yæs

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,8 @@
-
+import cv2 
 
 images = r"C:\Users\anne\Desktop\Daki\s2\projekter\miniprojekt_3\miniprojekt_3\Cropped and perspective corrected boards"
+
+# Convert to HSV
+
+hsv_images = cv2.cvtColor(images, cv2.COLOR_BGR2HSV)
+
