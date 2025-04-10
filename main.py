395653b68baee82dac5daf884b23f8e8d1d5e131
anneinger <anne@ingerslev.info>
2025-04-10 11:26:19 +0200

    doni

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import color_classification as cc
 
 def main():
-    image_path = r"C:\Users\anne\Desktop\Daki\s2\projekter\miniprojekt_3\miniprojekt_3\Cropped and perspective corrected boards"
-    label_path = r"C:\Users\anne\Desktop\Daki\s2\projekter\miniprojekt_3\labels_uden_kroner.csv"
+    image_path = r"miniprojekt_3\Cropped and perspective corrected boards"
+    label_path = r"labels_uden_kroner.csv"
 
     classifier = cc.Tile_Classifier()
     classifier.run_pipeline(image_path, label_path)
